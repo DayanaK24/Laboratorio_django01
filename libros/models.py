7bdfda20d6a6b9a69f261c1fc0818531983b031a
DayanaK24 <dcayo@unsa.edu.pe>
2024-11-26 23:13:52 -0500

    incluyendo nuevo tipo de dato a Libro

--- libros/models.py
+++ libros/models.py
@@ -7,3 +7,5 @@ class Libro(models.Model):
     genre = models.CharField(max_length=50)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
+    #rama pruebaLibros. agregando nuevo tipo de dato
+    cover_image = models.ImageField(upload_to='covers/', null=True, blank=True)
