973258c9f773d13846595db23d8ecfb89cf76a76
DayanaK24 <dcayo@unsa.edu.pe>
2024-11-26 23:02:18 -0500

    agregando el modelo al libros

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
