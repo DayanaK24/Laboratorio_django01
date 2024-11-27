6dfa19160c0ce908c2602d5b4bc0f67e6b3c3434
DayanaK24 <dcayo@unsa.edu.pe>
2024-11-26 23:03:09 -0500

    agregando el modelo al libros

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Para usar un hash seguro
+    created_at = models.DateTimeField(auto_now_add=True)
